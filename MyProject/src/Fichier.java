d3f0b4e160c58dba91241fc5609e77716de456c7
GauthierDefrance <defrance.gauthier@gmail.com>
2024-11-17 22:34:02 +0100

    Update Fichier.java

--- MyProject/src/Fichier.java
+++ MyProject/src/Fichier.java
@@ -1,5 +1,6 @@
 
 import com.drew.imaging.ImageMetadataReader;
+import com.drew.metadata.Directory;
 import com.drew.metadata.Metadata;
 import com.drew.metadata.exif.ExifSubIFDDirectory;
 import com.drew.metadata.exif.ExifIFD0Directory;
@@ -15,8 +16,22 @@ public class Fichier {
     private File imageFile;
     private Metadata metadata;
 
+    private String mime;
+    private String extension;
+    private String name;
+    private String date;
+    private String mdate;
+    private String lattitude;
+    private String longitude;
+    private String model;
+    private String desc;
+    private String size;
+
     private int height;
     private int width;
+    private int dpix;
+    private int dpiy;
+
 
     public Fichier(String path) {
         if(new File(path).exists()) {
@@ -28,19 +43,67 @@ public class Fichier {
 
     public String getPath(){ return path;}
 
-    public getMetadata() {
+    public void initMetadata() {
         try {
-            String text="";
-            // Charger le fichier image
-            File imageFile = new File("C:/Users/defra/Downloads/Canon_40D.jpg");
-
-            // Lire les métadonnées
-            Metadata metadata = ImageMetadataReader.readMetadata(imageFile);
-
-            // Parcourir et afficher les métadonnées
+            // Parcourir et stocker les métadonnées
             for (Directory directory : metadata.getDirectories()) {
                 for (Tag tag : directory.getTags()) {
-                    System.out.println(tag.getTagName() + " : " + tag.getDescription());
+                    switch (tag.getTagName()){
+                        case "Image Height":
+                            this.height = Integer.parseInt(tag.getDescription());
+                            break;
+                        case "Image Width":
+                            this.width = Integer.parseInt(tag.getDescription());
+                            break;
+
+                        case "Detected MIME Type":
+                            this.mime = tag.getDescription();
+                            break;
+
+                        case "File Name":
+                            this.name = tag.getDescription();
+                            break;
+
+                        case "Detected File Type Long Name":
+                            this.extension = tag.getDescription();
+                            break;
+
+                        case "File Size":
+                            this.size = ""+Integer.parseInt(tag.getDescription())/1000+"Ko";
+                            break;
+
+                        case "Latitude":
+                            this.lattitude = tag.getDescription();
+                            break;
+
+                        case "Longitude":
+                            this.longitude = tag.getDescription();
+                            break;
+                        case "Date/Time":
+                            this.date = tag.getDescription();
+                            break;
+                        case "File Modified Date":
+                            this.mdate = tag.getDescription();
+                            break;
+                        case "X Resolution":
+                            dpix = Integer.parseInt(tag.getDescription());
+                            break;
+                        case "Y Resolution":
+                            dpiy = Integer.parseInt(tag.getDescription());
+                            break;
+                        case "Model":
+                            model = tag.getDescription();
+                            break;
+                        case "Image Description":
+                            desc = Integer.parseInt(tag.getDescription());
+                            break;
+
+
+
+
+
+                    }
+                    tag.getDescription();
                 }
 
                 // Gérer les erreurs éventuelles
@@ -52,11 +115,7 @@ public class Fichier {
             }
         } catch (Exception e) {
             System.err.println("Erreur lors de l'extraction des métadonnées : " + e.getMessage());
-        }
-
+            }
     }
-
-
-
 
 }
