dde1aeb1bcfa570f2513c27c5361b556c4af3a99
GauthierDefrance <defrance.gauthier@gmail.com>
2024-12-21 00:27:31 +0100

    big maj GUI nearly finished

--- Metadata Extractor project Java v3/src/main/java/gui/GUI.java
+++ Metadata Extractor project Java v3/src/main/java/gui/GUI.java
@@ -1,6 +1,8 @@
 package gui;
 
 import Core.*;
+import com.sun.tools.javac.Main;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Image;
@@ -15,7 +17,7 @@ import java.util.Arrays;
 
 public class GUI extends JFrame {
 
-	private int page = 1;
+	private int page = 0;
 	private int searchSelector= 0;
 	private ArrayList<String> lstd = new ArrayList<String>();
 	private ArrayList<String> lstf = new ArrayList<String>();
@@ -45,6 +47,10 @@ public class GUI extends JFrame {
 		super(title);
 		init();
 
+		ImageIcon icon = new ImageIcon(".\\src\\main\\resources\\icon.png");
+		this.setIconImage(icon.getImage());
+
+
 		quitButton.addActionListener(new QuitAction(this));
 	}
 
@@ -53,18 +59,19 @@ public class GUI extends JFrame {
 		JPanel rightPanel = new JPanel();
 		JPanel leftPanel = new JPanel();
 		JMenuBar toptopPanel = new JMenuBar();
+		JMenu menuParametres = new JMenu("Menu SearchBy ...");
+		ButtonGroup buttonGroup = new ButtonGroup();
 		JPanel topbottomPanel = new JPanel();
 		JPanel topPanel = new JPanel();
 		JPanel bottomPanel = new JPanel();
 
-
 		centerPanel.setBorder(BorderFactory.createLineBorder(Color.black));
-		centerPanel.setBackground(Color.WHITE);
-
+		centerPanel.setBackground(Color.ORANGE);
 		up.setBackground(Color.DARK_GRAY);
 		up.setForeground(Color.WHITE);
 		down.setBackground(Color.DARK_GRAY);
 		down.setForeground(Color.WHITE);
+		leftPanel.setBackground(Color.DARK_GRAY);
 
 		Container contentPane = getContentPane();
 		contentPane.setLayout(border);
@@ -75,35 +82,81 @@ public class GUI extends JFrame {
 		topbottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
 		toptopPanel.add(parametres);
-		parametres.add(quitButton);
+		parametres.add(menuParametres);
 
+		JRadioButtonMenuItem item1 = new JRadioButtonMenuItem("Name");
+		JRadioButtonMenuItem item2 = new JRadioButtonMenuItem("Heigth");
+		JRadioButtonMenuItem item3 = new JRadioButtonMenuItem("MaxHeigth");
+		JRadioButtonMenuItem item4 = new JRadioButtonMenuItem("MinHeigth");
+		JRadioButtonMenuItem item5 = new JRadioButtonMenuItem("Width");
+		JRadioButtonMenuItem item6 = new JRadioButtonMenuItem("MaxWidth");
+		JRadioButtonMenuItem item7 = new JRadioButtonMenuItem("MinWidth");
+		JRadioButtonMenuItem item8 = new JRadioButtonMenuItem("Desc");
+		JRadioButtonMenuItem item9 = new JRadioButtonMenuItem("Date");
+		JRadioButtonMenuItem item10 = new JRadioButtonMenuItem("MaxSize");
+		JRadioButtonMenuItem item11 = new JRadioButtonMenuItem("MinSize");
+
+
+		toptopPanel.add(parametres);
+		parametres.add(menuParametres);
+		buttonGroup.add(item1);
+		menuParametres.add(item1);
+		buttonGroup.add(item2);
+		menuParametres.add(item2);
+		buttonGroup.add(item3);
+		menuParametres.add(item3);
+		buttonGroup.add(item4);
+		menuParametres.add(item4);
+		buttonGroup.add(item5);
+		menuParametres.add(item5);
+		buttonGroup.add(item6);
+		menuParametres.add(item6);
+		buttonGroup.add(item7);
+		menuParametres.add(item7);
+		buttonGroup.add(item8);
+		menuParametres.add(item8);
+		buttonGroup.add(item9);
+		menuParametres.add(item9);
+		buttonGroup.add(item10);
+		menuParametres.add(item10);
+		buttonGroup.add(item11);
+		menuParametres.add(item11);
+		parametres.addSeparator();
+		parametres.add(quitButton);
 		toptopPanel.add(help);
 		toptopPanel.add(search);
 		toptopPanel.add(searchT);
-
 		topbottomPanel.add(pathB2);
 		topbottomPanel.add(pathB);
 		topbottomPanel.add(pathT);
-
+		menu.add(option1);
+		menu.add(option2);
 		rightPanel.setLayout(new GridLayout(2, 1));
 		rightPanel.add(up);
 		rightPanel.add(down);
 
-
-		menu.add(option1);
-		menu.add(option2);
-
+		item1.addActionListener(new searchSelectorB(1));
+		item2.addActionListener(new searchSelectorB(2));
+		item3.addActionListener(new searchSelectorB(3));
+		item4.addActionListener(new searchSelectorB(4));
+		item5.addActionListener(new searchSelectorB(5));
+		item6.addActionListener(new searchSelectorB(6));
+		item7.addActionListener(new searchSelectorB(7));
+		item8.addActionListener(new searchSelectorB(8));
+		item9.addActionListener(new searchSelectorB(9));
+		item10.addActionListener(new searchSelectorB(10));
+		item11.addActionListener(new searchSelectorB(11));
 		up.addActionListener(new upAction());
 		down.addActionListener(new downAction());
 		pathB.addActionListener(new ppathupdate());
 		pathB2.addActionListener(new pathremonter());
 		search.addActionListener(new searchf());
 
-		String path= "C:\\Users\\defra\\Pictures\\Screenshots";
+		String path =System.getProperty("user.dir"); //Renvoit l'endroit depuis ou est lancé le répertoire
 		pathT.setText(path);
 
 		File file = new File(path);
-		if (file.exists()&&file.isDirectory()) {
+		if (file.exists() && file.isDirectory()) {
 			Folder folder = new Folder(path);
 
 			for (File f : folder.getFolders()) {
@@ -131,6 +184,8 @@ public class GUI extends JFrame {
 		setVisible(true);
 	}
 
+
+
 	private void updateTb() {
 		centerPanel.removeAll();
 		centerPanel.setLayout(new GridLayout(4, 8, 5, 5));
@@ -138,7 +193,7 @@ public class GUI extends JFrame {
 
 		for (int i = 0; i < 4; i++) {
 			for (int j = 0; j < 8; j++) {
-				if(lst.size()>p) {
+				if (lst.size() > p) {
 					// Récupérer le nom du fichier depuis le chemin
 					String fileName = Paths.get(lst.get(p)).getFileName().toString();
 					tb[i][j] = new JButton(fileName); // Nom des boutons
@@ -146,14 +201,13 @@ public class GUI extends JFrame {
 					tb[i][j].setHorizontalTextPosition(SwingConstants.CENTER); // Texte centré horizontalement
 					tb[i][j].setVerticalTextPosition(SwingConstants.BOTTOM);  // Texte en bas
 
-					if(!(lstd.size()<=p)){
-						ImageIcon icon1 = new ImageIcon("D:\\github\\Images-metadonnees-avec-Java\\Metadata Extractor project Java v3\\src\\main\\resources\\folder.png");
+					if (!(lstd.size() <= p)) {
+						ImageIcon icon1 = new ImageIcon(".\\src\\main\\resources\\icon.png");
 						Image img = icon1.getImage();
 						Image scaledImg = img.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
 						tb[i][j].setIcon(new ImageIcon(scaledImg));
 						tb[i][j].addMouseListener(new DoubleClicListenerD(lst.get(p))); // Ajouter le même ActionListener à chaque
-					}
-					else {
+					} else {
 						ImageIcon icon2 = new ImageIcon(lst.get(p));
 						Image img = icon2.getImage();
 						Image scaledImg = img.getScaledInstance(150, 150, Image.SCALE_SMOOTH);
@@ -161,7 +215,9 @@ public class GUI extends JFrame {
 					}
 					// Ajouter un MouseListener pour détecter le double-clic et le clic droit
 					tb[i][j].addMouseListener(new ClicDroitListener(menu));
-					tb[i][j].setBackground(Color.WHITE);
+
+					tb[i][j].setBackground(new Color(128, 128, 128));
+
 					centerPanel.add(tb[i][j]); // Ajouter chaque bouton au panel
 				}
 				p++;
@@ -179,14 +235,14 @@ public class GUI extends JFrame {
 		lst.clear();
 		//Création de l'objet folder à l'endroit indiqué
 		File file = new File(path);
-		if (file.exists()&&file.isDirectory()) {
+		if (file.exists() && file.isDirectory()) {
 			Folder folder = new Folder(path);
 
-			for(File f : folder.getFolders()) {
+			for (File f : folder.getFolders()) {
 				lstd.add(f.getAbsolutePath());
 			}
 
-			for(File f : folder.getImages()) {
+			for (File f : folder.getImages()) {
 				lstf.add(f.getAbsolutePath());
 			}
 			lst.addAll(lstd);
@@ -196,51 +252,47 @@ public class GUI extends JFrame {
 		centerPanel.updateUI();
 	}
 
-	// Classe privée pour gérer le clic droit et afficher le JPopupMenu
-	private class ClicDroitListener extends MouseAdapter {
-		private final JPopupMenu menu;
-
-		public ClicDroitListener(JPopupMenu menu) {
-			this.menu = menu;
-		}
+	private class ppathupdate implements ActionListener {
 
 		@Override
-		public void mouseClicked(MouseEvent e) {
-			if (e.getButton() == MouseEvent.BUTTON3) {  // Clic droit
-				menu.show(e.getComponent(), e.getX(), e.getY());
-
-			}
+		public void actionPerformed(ActionEvent e) {
+			page = 0;
+			pathupdate();
+			centerPanel.updateUI();
 		}
+
 	}
 
-	// Classe privée pour gérer le double-clic
-	private class DoubleClicListenerD extends MouseAdapter {
-		private String path;
-		public DoubleClicListenerD(String path) {
-		this.path = path;
-		}
+	private class pathremonter implements ActionListener {
 
 		@Override
-		public void mousePressed(MouseEvent e) {
-			if (e.getClickCount() == 2) {  // Double-clic détecté
-				pathT.setText(path);
-				page = 0;
-				pathupdate();
+		public void actionPerformed(ActionEvent e) {
+			String path = pathT.getText();//donne le path actuel
+			File file = new File(path);
+			if (file.exists()&&file.isDirectory()) {
+				try {
+					String parent = Paths.get(pathT.getText()).getParent().toString(); //Donne le parent
+					pathT.setText(parent);
+					page = 0;
+					pathupdate();
+				} catch (Exception ex) {}
 			}
 			centerPanel.updateUI();
 		}
 	}
 
+	private class searchSelectorB implements ActionListener {
+		private int selector;
 
-	private class upAction implements ActionListener {
+		public searchSelectorB(int selector) {
+			this.selector = selector;
+		}
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if (page > 0) {
-				page--;
-				updateTb();
-				centerPanel.updateUI();
-			}
+			searchSelector = this.selector;
+			updateTb();
+			centerPanel.updateUI();
 		}
 
 	}
@@ -297,7 +349,7 @@ public class GUI extends JFrame {
 					lst.clear();
 					lstf.clear();
 					lstd.clear();
-
+					page = 0;
 					for (File f : images) {
 						lst.add(f.getAbsolutePath());
 					}
@@ -311,46 +363,67 @@ public class GUI extends JFrame {
 		}
 	}
 
+	private class upAction implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (page > 0) {
+				page--;
+				updateTb();
+				centerPanel.updateUI();
+			}
+		}
+
+	}
+
 	private class downAction implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if(lst.size()>(page+1) * 32){
-			page++;
-			updateTb();
-			centerPanel.updateUI();
+				page++;
+				updateTb();
+				centerPanel.updateUI();
 			}
 		}
 
 	}
 
-	private class ppathupdate implements ActionListener {
+	// Classe privée pour gérer le clic droit et afficher le JPopupMenu
+	private class ClicDroitListener extends MouseAdapter {
+		private final JPopupMenu menu;
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			pathupdate();
-			centerPanel.updateUI();
+		public ClicDroitListener(JPopupMenu menu) {
+			this.menu = menu;
 		}
 
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getButton() == MouseEvent.BUTTON3) {  // Clic droit
+				menu.show(e.getComponent(), e.getX(), e.getY());
+
+			}
+		}
 	}
 
-	private class pathremonter implements ActionListener {
+	// Classe privée pour gérer le double-clic
+	private class DoubleClicListenerD extends MouseAdapter {
+		private String path;
+
+		public DoubleClicListenerD(String path) {
+			this.path = path;
+		}
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
-			String path = pathT.getText();//donne le path actuel
-			File file = new File(path);
-			if (file.exists()&&file.isDirectory()) {
-				try {
-					String parent = Paths.get(pathT.getText()).getParent().toString(); //Donne le parent
-					pathT.setText(parent);
-					pathupdate();
-				} catch (Exception ex) {}
+		public void mousePressed(MouseEvent e) {
+			if (e.getClickCount() == 2) {  // Double-clic détecté
+				pathT.setText(path);
+				page = 0;
+				pathupdate();
 			}
 			centerPanel.updateUI();
 		}
 	}
-
 
 	private class QuitAction implements ActionListener {
 		//Window to be closed.
@@ -369,5 +442,6 @@ public class GUI extends JFrame {
 
 	public static void main(String[] args) {
 		new GUI("GUI_C4");
+
 	}
 }
