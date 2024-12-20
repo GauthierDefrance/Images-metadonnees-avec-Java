e322c0523fd53f8be10bfd5da49b3ca9b01ac038
GauthierDefrance <defrance.gauthier@gmail.com>
2024-12-20 15:35:06 +0100

    Create order.java

---
+++ Metadata Extractor project Java v3/src/main/java/Core/order.java
@@ -0,0 +1,23 @@
+package Core;
+
+import java.util.ArrayList;
+
+public class order {
+    private ArrayList<String> list=new ArrayList<String>();
+
+    public order(String listUnTreated){
+        String[] tmp=listUnTreated.split("\n");
+
+        list
+
+
+    }
+
+
+
+
+
+
+
+
+}
