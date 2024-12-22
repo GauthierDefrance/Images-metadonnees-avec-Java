5847c5754d5dbd2dd8fb69e081150979e2a5b792
tartinax <pokemon.furtif@gmail.com>
2024-12-22 21:55:56 +0100

    Update GUI.java

--- Metadata Extractor project Java v3/src/main/java/gui/GUI.java
+++ Metadata Extractor project Java v3/src/main/java/gui/GUI.java
@@ -1,19 +1,14 @@
 package gui;
 
 import Core.*;
-import com.drew.imaging.ImageProcessingException;
-import com.sun.tools.javac.Main;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -25,27 +20,19 @@ public class GUI extends JFrame {
 	private int page = 0;
 	private int searchSelector= 0;
 	private String orderSelector = "name";
-	private Boolean orderBool = Boolean.FALSE;
+	private Boolean orderBool = false;
+
 	private ArrayList<String> lstd = new ArrayList<String>();
 	private ArrayList<String> lstf = new ArrayList<String>();
 	private ArrayList<String> lst = new ArrayList<String>();
-	private JButton help = new JButton("help ?");
-	private JButton order = new JButton("Order:");
-	private JButton search = new JButton("Search :");
-	private JButton pathB = new JButton("Path :");
-	private JButton pathB2 = new JButton("←");
-	private JButton up = new JButton("↑");
-	private JButton down = new JButton("↓");
 
 	private JButton[][] tb = new JButton[4][8];
 	private JPanel centerPanel = new JPanel();
 
-	private JMenu parametres = new JMenu("Parametres");
 	private JTextField pathT = new JTextField(50);
 	private JTextField searchT = new JTextField(50);
 	private JTextField snapchotSave = new JTextField(50);
 	private JTextField snapchotCompare = new JTextField(50);
-	private JMenuItem quitButton = new JMenuItem("quit");
 
 
 	public GUI(String title) {
@@ -54,13 +41,9 @@ public class GUI extends JFrame {
 
 		ImageIcon icon = new ImageIcon(".\\src\\main\\resources\\icon.png");
 		this.setIconImage(icon.getImage());
-
-
-		quitButton.addActionListener(new QuitAction(this));
 	}
 
 	private void init() {
-
 		BorderLayout border = new BorderLayout();
 		JPanel rightPanel = new JPanel();
 		JPanel leftPanel = new JPanel();
@@ -69,9 +52,22 @@ public class GUI extends JFrame {
 		JPanel bottomPanel = new JPanel();
 
 		JMenuBar toptopPanel = new JMenuBar();
-		JMenu menuParametres = new JMenu("Menu SearchBy ...");
-		JMenu orderByParametres = new JMenu("Menu OrderBy ...");
+
+		JMenu parametres = new JMenu("Parametres");
+		JMenu menuSearchByParametres = new JMenu("Menu SearchBy ...");
+		JMenu menuOrderByParametres = new JMenu("Menu OrderBy ...");
 		JMenu menuSnapchotParametres = new JMenu("Menu Snapchot ...");
+		JMenuItem quitButton = new JMenuItem("quit");
+
+		JButton help = new JButton("help ?");
+		JButton order = new JButton("Order:");
+		JButton search = new JButton("Search :");
+		JButton pathButton = new JButton("Path :");
+		JButton pathParent = new JButton("←");
+		JButton up = new JButton("↑");
+		JButton down = new JButton("↓");
+		JButton snapchotSaveButton = new JButton("snapchotSave:");
+		JButton snapchotCompareButton = new JButton("snapchotConpare:");
 		ButtonGroup buttonGroup = new ButtonGroup();
 		ButtonGroup buttonGroup2 = new ButtonGroup();
 
@@ -85,17 +81,32 @@ public class GUI extends JFrame {
 
 		Container contentPane = getContentPane();
 		contentPane.setLayout(border);
-
 		topPanel.setLayout(new GridLayout(2, 1));
 		rightPanel.setLayout(new GridLayout(2, 1));
-		topPanel.add(toptopPanel);
-		topPanel.add(topbottomPanel);
 		topbottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
 
+		topPanel.add(toptopPanel);
+		topPanel.add(topbottomPanel);
 		toptopPanel.add(parametres);
-		parametres.add(menuParametres);
-		parametres.add(orderByParametres);
+		toptopPanel.add(help);
+		toptopPanel.add(search);
+		toptopPanel.add(searchT);
+		topbottomPanel.add(pathParent);
+		topbottomPanel.add(pathButton);
+		topbottomPanel.add(pathT);
+		rightPanel.add(up);
+		rightPanel.add(down);
+
+		parametres.add(menuSearchByParametres);
+		parametres.add(menuOrderByParametres);
 		parametres.add(menuSnapchotParametres);
+
+		menuSnapchotParametres.add(snapchotSaveButton);
+		menuSnapchotParametres.add(snapchotSave);
+		menuSnapchotParametres.add(snapchotCompareButton);
+		menuSnapchotParametres.add(snapchotCompare);
+		parametres.addSeparator();
+		parametres.add(quitButton);
 
 		JRadioButtonMenuItem itemorBOOL = new JRadioButtonMenuItem("reverse");
 		JRadioButtonMenuItem item1or = new JRadioButtonMenuItem("Name");
@@ -115,55 +126,43 @@ public class GUI extends JFrame {
 		JRadioButtonMenuItem item10 = new JRadioButtonMenuItem("MaxSize");
 		JRadioButtonMenuItem item11 = new JRadioButtonMenuItem("MinSize");
 
-		menuSnapchotParametres.add(snapchotSave);
-		menuSnapchotParametres.add(snapchotCompare);
 
-		orderByParametres.add(order);
-		orderByParametres.add(itemorBOOL);
+		menuOrderByParametres.add(order);
+		menuOrderByParametres.add(itemorBOOL);
 		buttonGroup2.add(item1or);
-		orderByParametres.add(item1or);
+		menuOrderByParametres.add(item1or);
 		buttonGroup2.add(item2or);
-		orderByParametres.add(item2or);
+		menuOrderByParametres.add(item2or);
 		buttonGroup2.add(item3or);
-		orderByParametres.add(item3or);
+		menuOrderByParametres.add(item3or);
 		buttonGroup2.add(item4or);
-		orderByParametres.add(item4or);
+		menuOrderByParametres.add(item4or);
 		buttonGroup2.add(item5or);
-		orderByParametres.add(item5or);
-
+		menuOrderByParametres.add(item5or);
 
 		buttonGroup.add(item1);
-		menuParametres.add(item1);
+		menuSearchByParametres.add(item1);
 		buttonGroup.add(item2);
-		menuParametres.add(item2);
+		menuSearchByParametres.add(item2);
 		buttonGroup.add(item3);
-		menuParametres.add(item3);
+		menuSearchByParametres.add(item3);
 		buttonGroup.add(item4);
-		menuParametres.add(item4);
+		menuSearchByParametres.add(item4);
 		buttonGroup.add(item5);
-		menuParametres.add(item5);
+		menuSearchByParametres.add(item5);
 		buttonGroup.add(item6);
-		menuParametres.add(item6);
+		menuSearchByParametres.add(item6);
 		buttonGroup.add(item7);
-		menuParametres.add(item7);
+		menuSearchByParametres.add(item7);
 		buttonGroup.add(item8);
-		menuParametres.add(item8);
+		menuSearchByParametres.add(item8);
 		buttonGroup.add(item9);
-		menuParametres.add(item9);
+		menuSearchByParametres.add(item9);
 		buttonGroup.add(item10);
-		menuParametres.add(item10);
+		menuSearchByParametres.add(item10);
 		buttonGroup.add(item11);
-		menuParametres.add(item11);
-		parametres.addSeparator();
-		parametres.add(quitButton);
-		toptopPanel.add(help);
-		toptopPanel.add(search);
-		toptopPanel.add(searchT);
-		topbottomPanel.add(pathB2);
-		topbottomPanel.add(pathB);
-		topbottomPanel.add(pathT);
-		rightPanel.add(up);
-		rightPanel.add(down);
+		menuSearchByParametres.add(item11);
+
 
 		itemorBOOL.addActionListener(new orderReverse());
 		item1or.addActionListener(new orderSelectorB("name"));
@@ -183,10 +182,11 @@ public class GUI extends JFrame {
 		item9.addActionListener(new searchSelectorB(9));
 		item10.addActionListener(new searchSelectorB(10));
 		item11.addActionListener(new searchSelectorB(11));
+
 		up.addActionListener(new upAction());
 		down.addActionListener(new downAction());
-		pathB.addActionListener(new ppathupdate());
-		pathB2.addActionListener(new pathremonter());
+		pathButton.addActionListener(new ppathupdate());
+		pathParent.addActionListener(new pathremonter());
 		search.addActionListener(new searchf());
 		order.addActionListener(new orderf());
 		help.addActionListener(new helpPopup());
@@ -218,6 +218,7 @@ public class GUI extends JFrame {
 		contentPane.add(BorderLayout.WEST, leftPanel);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		quitButton.addActionListener(new QuitAction(this));
 		pack();
 		setLocationRelativeTo(null);
 		setSize(720, 480);
@@ -448,10 +449,10 @@ public class GUI extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			if (orderBool) {
-				orderBool = Boolean.FALSE;
+				orderBool = false;
 			}
 			else {
-				orderBool = Boolean.TRUE;
+				orderBool = true;
 			}
 		}
 
@@ -607,7 +608,7 @@ public class GUI extends JFrame {
 					Merci d'avoir lu l'aide
 					Par Kennan & Gauthier
 					"""
-					);
+			);
 		}
 
 	}
